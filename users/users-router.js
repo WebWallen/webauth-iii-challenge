b19b52a4b19a37c1cbbf3ff22aa8cdfd6931fb57
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-16 17:37:56 -0400

    Cut the department middleware so it's working again.

--- users/users-router.js
+++ users/users-router.js
@@ -4,7 +4,7 @@ const Users = require('./user-model');
 const restricted = require('../auth/restricted-middleware');
 const checkDept = require('../auth/check-department-middleware');
 
-router.get('/', restricted, checkDept("Lord of the Doggos"), (req, res) => {
+router.get('/', restricted, (req, res) => {
     Users.find()
         .then(users => res.status(200).json({ users }))
         .catch(err => res.send(err))
