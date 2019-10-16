7fec6996ec892c3fb368ca09a838ceeb0898e9e1
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-16 17:25:10 -0400

    Debugged users path, MVP met.

--- users/users-router.js
+++ users/users-router.js
@@ -5,7 +5,7 @@ const restricted = require('../auth/restricted-middleware');
 
 router.get('/', restricted, (req, res) => {
     Users.find()
-        .then(users => res.status({ currentUser: req.username, users }))
+        .then(users => res.status(200).json({ users }))
         .catch(err => res.send(err))
 });
 
