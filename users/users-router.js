01f41068611f945053274f95e1fd61cad956c8df
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-16 17:36:25 -0400

    Added checkDepartment middleware but it doesn't do what it's supposed to despite matching the string exactly... shrug.

--- users/users-router.js
+++ users/users-router.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 
 const Users = require('./user-model');
 const restricted = require('../auth/restricted-middleware');
+const checkDept = require('../auth/check-department-middleware');
 
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, checkDept("Lord of the Doggos"), (req, res) => {
     Users.find()
         .then(users => res.status(200).json({ users }))
         .catch(err => res.send(err))
